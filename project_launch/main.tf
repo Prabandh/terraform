695ffb8dba146b7ed457223c933c1cdfa2b8ac00
root <root@ip-172-31-2-134.us-west-1.compute.internal>
2022-07-23 16:25:13 +0000

    name change

--- project_launch/main.tf
+++ project_launch/main.tf
@@ -1,5 +1,5 @@
 module "instance" {
- source = "https://github.com/Prabandh/terraform/tree/master/module"
+ source = "https://github.com/Prabandh/terraform.git"
 ami= "ami-01154c8b2e9a14885"
 instance_type="t2.micro"
 key_name="abc"
